409f3895685a600fa6ffc82b203791ad83af2237
Pavel Popov <legeon48@mail.ru>
2022-04-08 20:48:52 +0300

    Создание модели данных

---
+++ NewPortal.Server.Lib/Models/News.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NewPortal.Server.Lib.Models
+{
+    public class News
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime DateOfCreation  { get; set; }
+        public int AuthorId { get; set; }
+    }
+}
