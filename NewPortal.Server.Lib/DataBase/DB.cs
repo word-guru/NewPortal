8d59d55d1bf50bc4fa6837037380e909524a6453
Pavel Popov <legeon48@mail.ru>
2022-04-08 21:32:26 +0300

    Добавление класса ДБ

---
+++ NewPortal.Server.Lib/DataBase/DB.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Dapper;
+using MySql.Data.MySqlClient;
+using NewPortal.Server.Lib.Models;
+
+namespace NewPortal.Server.Lib.DataBase
+{
+    public class DB
+    {
+        private MySqlConnection _connection;
+
+        public DB()
+        {
+            var conn = File.ReadAllTextAsync("connection_to_db.txt").Result;
+            _connection = new MySqlConnection(conn);
+        }
+
+        public async Task<IEnumerable<News>> GetAllNews()
+        {
+            await _connection.OpenAsync();
+
+            var sql = "SELECT id, title, content, date_of_creation, author FROM tab_news";
+
+            var result = await _connection.QueryAsync<News>(sql);
+            
+            await _connection.CloseAsync();
+
+            return result;
+        }
+    }
+}
