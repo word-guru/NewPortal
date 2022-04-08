7cc7a1e025bbf2edd69683624566899e1a10b806
Pavel Popov <legeon48@mail.ru>
2022-04-08 20:27:32 +0300

    Добавление SQL файла для создания таблицы

---
+++ SQL/create_tables.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE tab_users(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    login VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    first_name VARCHAR(125) NOT NULL,
+    last_name VARCHAR(125) NOT NULL,
+    middle_name VARCHAR(125) 
+);
+
+CREATE TABLE tab_news (
+  id INT not null PRIMARY KEY AUTO_INCREMENT,
+  title VARCHAR(255) NOT NULL,
+  content TEXT NOT NULL, 
+   date_of_creation DATETIME NOT NULL,
+   author INT NOT NULL,
+   FOREIGN KEY (author) REFERENCES tab_users(id) ON UPDATE NO ACTION 
+                                                 ON DELETE NO ACTION 
+);
