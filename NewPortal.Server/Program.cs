4baf663bebd7365c722816d570c20e18dbd53c1b
Pavel Popov <legeon48@mail.ru>
2022-04-15 19:54:50 +0300

    Создание сервера

--- NewPortal.Server/Program.cs
+++ NewPortal.Server/Program.cs
@@ -1,12 +1,74 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using NewPortal.Server.Lib.DataBase;
+using NewPortal.Server.Lib.Models;
 
 namespace NewPortal.Server
 {
     class Program
     {
-        static void Main(string[] args)
+        private static async Task Main()
         {
-            Console.WriteLine("Hello World!");
+            var server = new TcpListener(IPAddress.Loopback, 8005);
+            server.Start();
+
+            while (true)
+            {
+                var client = await server.AcceptTcpClientAsync();
+                var clientStream = client.GetStream();
+
+                var requestRaw = await GetRequest(clientStream);
+                var request = JsonSerializer.Deserialize<Request>(requestRaw);
+
+                switch (request?.Type)
+                {
+                    case "news_all":
+                        await GetAllNews(clientStream);
+                        break;
+                    case "user_all":
+                        break;
+                    case "news":
+                        break;
+                    case "insert_news":
+                        break;
+                }
+            }
+        }
+
+        private static async Task GetAllNews(Stream stream)
+        {
+            var db = new DB();
+            var news = await db.GetAllNews();
+
+            SendData(news, stream);
+        }
+
+        private static async Task SendData(object value, Stream stream)
+        {
+            var sendRaw = JsonSerializer.Serialize(value,value.GetType());
+            var data = Encoding.Unicode.GetBytes(sendRaw);
+            await stream.WriteAsync(data, 0, data.Length);
+        }
+        
+        private static async Task<string> GetRequest(NetworkStream stream)
+        {
+            var builder = new StringBuilder();
+            int bytes = 0;
+            var data = new byte[64];
+            do
+            {
+                bytes = await stream.ReadAsync(data, 0, data.Length);
+                builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+            } while (stream.DataAvailable);
+            
+            return builder.ToString();
         }
     }
 }
---
+++ NewPortal.Server/Request.cs
@@ -0,0 +1,13 @@
+﻿#nullable enable
+using NewPortal.Server.Lib.Models;
+
+namespace NewPortal.Server
+{
+    public class Request
+    {
+        public string Type { get; set; }
+        public int? Id { get; set; }
+        
+        public News? News { get; set; }
+    }
+}
